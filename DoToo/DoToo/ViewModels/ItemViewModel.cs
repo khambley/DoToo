d61a22aaa38c70db451524d0c9e6229d02436dad
Katherine Hambley <superdesigngirl@mac.com>
2021-08-05 10:59:40 -0500

    60-62 Added the MainViewModel, ToDoItemVM, and ItemVM

---
+++ DoToo/DoToo/ViewModels/ItemViewModel.cs
@@ -0,0 +1,17 @@
+﻿using DoToo.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DoToo.ViewModels
+{
+	public class ItemViewModel : ViewModel
+	{
+		private readonly ToDoItemRepository repository;
+
+		public ItemViewModel(ToDoItemRepository repository)
+		{
+			this.repository = repository;
+		}
+	}
+}
---
+++ DoToo/DoToo/ViewModels/ToDoItemViewModel.cs
@@ -0,0 +1,16 @@
+﻿using DoToo.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DoToo.ViewModels
+{
+	public class ToDoItemViewModel : ViewModel
+	{
+		public ToDoItemViewModel(ToDoItem item) => Item = item;
+
+		public event EventHandler ItemStatusChanged;
+		public ToDoItem Item { get; private set; }
+		public string StatusText => Item.Completed ? "Reactivate" : "Completed";
+	}
+}
